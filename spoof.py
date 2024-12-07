6459a56af83d298d05c8d1e48938b47b0fcc89e5
stephmukami <stephmukami@gmail.com>
2024-12-07 09:59:34 +0300

    feature:appended hexstring to the hashed file

--- spoof.py
+++ spoof.py
@@ -10,13 +10,17 @@
 # 6. Save the modified image as "altered.jpg".
 # 7. Verify the hash of "altered.jpg" starts with "0x24".
 # 8. Output the adjusted hash and file name.
-
+"""
+remember to check usecases eg if there arent enough args
+"""
 import sys
 import hashlib
 
-def hashfile(file):
+def hashfile(hexstring,file):
     BUFFER_SIZE = 65536 # confirm the average file size 
     sha256 = hashlib.sha256()
+
+    decimal_value = int(hexstring,16)
 
     with open(file,"rb") as f: #what does rb mean,must the file be hardcoded
         while True:
@@ -26,8 +30,9 @@ def hashfile(file):
                 break
 
             sha256.update(data)
+    new_hash = str(decimal_value) + str(sha256.hexdigest())
 
-    return sha256.hexdigest()
+    return new_hash
 
-file_hash = hashfile(sys.argv[1])
-print(f"Hashed file is {file_hash}")
+file_hash = hashfile(sys.argv[1],sys.argv[2])
+print(f" Newly Hashed file is {file_hash}")
